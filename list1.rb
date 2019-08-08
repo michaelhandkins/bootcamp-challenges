6ca0a64a1f3e797bca8a4f979384ef6600752616
Michael Handkins <michaelhandkins@aol.com>
2019-08-08 01:34:49 +0000

    Linked list 1

--- list1.rb
+++ list1.rb
@@ -1,24 +1,63 @@
-class Stack
-    attr_reader :data, :value
+class LinkedListNode
+  attr_accessor :value, :next_node
 
-    def initialize
-        @data = nil
-    end
-
-    # Push a value onto the stack
-    def push(value)
-        @value = value
-        print "#{@value} --> #{@data}"
-    end
-
-    # Pop an item off the stack.
-    # Remove the last item that was pushed onto the
-    # stack and return the value to the user
-    def pop
-        # I RETURN A VALUE
-    end
-
+  def initialize(value, next_node=nil)
+    @value = value
+    @next_node = next_node
+  end
 end
 
-Stack.new
-Stack.push(3)
+def print_values(list_node)
+  print "#{list_node.value} --> "
+  if list_node.next_node.nil?
+    print "nil\n"
+    return
+  else
+    print_values(list_node.next_node)
+  end
+end
+
+class Stack
+  attr_reader :data
+
+  def initialize
+    @data = nil
+  end
+
+  # Push an item onto the stack
+  def push(element)
+    @data = LinkedListNode.new(element, @data)
+  end
+
+  # Pop an item off the stack.
+  # Remove the last item that was pushed onto the
+  # stack and return it to the user
+  # def pop
+  #   ret = @data.value
+  #   @data = @data.next_node
+  #   ret
+  # end
+end
+
+def reverse_list(list_item)
+  stack = Stack.new
+
+  while list_item
+    stack.push(list_item.value)
+    list_item = list_item.next_node
+  end
+
+  stack.data
+end
+
+node1 = LinkedListNode.new(37)
+node2 = LinkedListNode.new(99, node1)
+node3 = LinkedListNode.new(12, node2)
+
+print_values(node3)
+
+puts "-------"
+
+revlist = reverse_list(node3)
+
+print_values(revlist)
